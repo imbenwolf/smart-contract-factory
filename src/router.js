ddfbf025e13889fbb81c1b41f383e61c7c3f94b7
Wolf Benjamin, GHR-OSA-NEX-21 <Benjamin.Wolf@swisscom.com>
2019-03-28 14:02:55 +0100

    Add routes of app

--- src/router.js
+++ src/router.js
@@ -1,6 +1,5 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Home from "./views/Home.vue";
 
 Vue.use(Router);
 
@@ -10,17 +9,29 @@ export default new Router({
   routes: [
     {
       path: "/",
-      name: "home",
-      component: Home
+      name: "contracts",
+      component: null
     },
     {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
+      path: "/proxies",
+      name: "proxies",
+      component: null
+    },
+    {
+      path: "/create",
+      component: null,
+      children: [
+        {
+          path: "erc20",
+          name: "erc20",
+          component: null
+        },
+        {
+          path: "erc721",
+          name: "erc721",
+          component: null
+        }
+      ]
     }
   ]
 });
