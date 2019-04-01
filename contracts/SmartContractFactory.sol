6ecf21d9739a8f07bdc17e52c6b48059fc1de31b
Wolf Benjamin, GHR-OSA-NEX-21 <Benjamin.Wolf@swisscom.com>
2019-04-01 11:20:11 +0200

    Add functionality to create (proxy) smart contract and save public smart contract with input validation in backend smart contract

--- contracts/SmartContractFactory.sol
+++ contracts/SmartContractFactory.sol
@@ -5,16 +5,76 @@ import "zos-lib/contracts/application/App.sol";
 
 contract SmartContractFactory is Initializable {
     App private app;
+    string private defaultPackageName;
+
+    mapping(address => Contract[]) private userContracts;
+
+    struct Contract {
+        address contractAddress;
+        string contractName;
+        bool isContractAdmin;
+    }
 
     function initialize(App _app) public initializer {
         app = _app;
+        defaultPackageName = "openzeppelin-eth";
     }
 
-    function getImplementation(string packageName, string contractName)
+    function getImplementation(string contractName)
         public
         view
         returns (address)
     {
-        return app.getImplementation(packageName, contractName);
+        require(isContractSupported(contractName), "contract not supported");
+
+        return app.getImplementation(defaultPackageName, contractName);
+    }
+
+    function createSmartContract(string contractName, bytes _data) public {
+        require(_data.length != 0, "data is required");
+        require(isContractSupported(contractName), "contract not supported");
+
+        address contractAddress = app.create(
+            defaultPackageName,
+            contractName,
+            msg.sender,
+            _data
+        );
+
+        address owner = msg.sender;
+        Contract[] storage contracts = userContracts[owner];
+        Contract memory newContract = Contract(
+            contractAddress,
+            contractName,
+            true
+        );
+        contracts.push(newContract);
+    }
+
+    function saveSmartContract(address contractAddress, string contractName)
+        public
+    {
+        require(isContractSupported(contractName), "contract not supported");
+
+        address owner = msg.sender;
+        Contract[] storage contracts = userContracts[owner];
+        Contract memory newContract = Contract(
+            contractAddress,
+            contractName,
+            false
+        );
+        contracts.push(newContract);
+    }
+
+    function isContractSupported(string contractName)
+        private
+        pure
+        returns (bool)
+    {
+        return keccak256(abi.encodePacked(contractName)) == keccak256(
+            abi.encodePacked("StandaloneERC20")
+        ) || keccak256(abi.encodePacked(contractName)) == keccak256(
+            abi.encodePacked("StandaloneERC721")
+        );
     }
 }
