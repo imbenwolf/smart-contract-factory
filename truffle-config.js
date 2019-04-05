bfca44e94352248599149c69851bbe27b85f07c1
Wolf Benjamin, GHR-OSA-NEX-21 <Benjamin.Wolf@swisscom.com>
2019-04-05 17:11:13 +0200

    Add ability to deploy to rinkeby test net for ethereum and add zeppelinos rinkeby network file

--- truffle-config.js
+++ truffle-config.js
@@ -25,7 +25,7 @@
 // const mnemonic = fs.readFileSync(".secret").toString().trim();
 
 const HDWalletProvider = require("truffle-hdwallet-provider");
-const { ropsten } = require("./network-config.js");
+const config = require("./network-config.js");
 
 module.exports = {
   /**
@@ -71,14 +71,23 @@ module.exports = {
     ropsten: {
       provider: () =>
         new HDWalletProvider(
-          ropsten.mnemonicPhrase,
-          `https://ropsten.infura.io/v3/${ropsten.infuraKey}`
+          config.mnemonicPhrase,
+          `https://ropsten.infura.io/v3/${config.infuraKey}`
         ),
       network_id: 3, // Ropsten's id
       gas: 5500000, // Ropsten has a lower block limit than mainnet
       confirmations: 2, // # of confs to wait between deployments. (default: 0)
       timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
       skipDryRun: true // Skip dry run before migrations? (default: false for public nets )
+    },
+    rinkeby: {
+      provider: () =>
+        new HDWalletProvider(
+          config.mnemonicPhrase,
+          `https://rinkeby.infura.io/v3/${config.infuraKey}`
+        ),
+      network_id: 4,
+      skipDryRun: true
     }
 
     // Useful for private networks
